20b9ee7d13ce92f67b9aadf55479dcd9fe132f8b
MirzaAyon <ayonmirza98@gmail.com>
2022-07-28 13:47:46 +0600

    two errors added with null and undefined

---
+++ Music Manager with bug solution/bug-report.js
@@ -0,0 +1,23 @@
+//aurora diye search korle error ashche line 9 js 
+//console.log(keyword); ekhane undefined ashtese
+//type error undefined e value read korte partese na
+//2 no line e console.log(id) etao show hochee
+//tar mane logical error ache 
+//elementById ke return korinai
+//return krsi ekhn aurora er 4 ta item showing
+//html tao showing
+
+
+//ebar 37 no line e null er sathe ekta appendchild hochee ja pachee na 
+//artistContainer e problem
+//etake console.log korbo
+//line 17 te console.log(artistContainer); dibo
+//con+f diye artistcontainer er onnanno func gulakeo khujbo
+//line 17 te null ashtese
+//const artistContainer = elementById("artist"); dewa ache but artists hbe 
+//ebar thik thak moto artist take pabe and inspact e show hbe 
+//ui teo coldplay ashbe 
+
+
+
+//ekhn every pic e hover korle ekta album btn ashbe shetake click korle r album show hochee na 
